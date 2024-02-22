211e13914689344aa7cd21e3d02c68a6a6fc56c6
hitaishmd <hitaish@noesyssoftware.com>
2024-02-22 13:17:48 +0530

    new commit

--- react-movies/src/App.tsx
+++ react-movies/src/App.tsx
@@ -51,12 +51,12 @@ function App() {
 
     
     return (
-        <>
+        <div className="container">
           <h3>In Theaters</h3>
           <MoviesList movies={movies.inTheaters}/>
           <h3>Upcoming Releases</h3>
           <MoviesList movies={movies.upcomingReleases}/>
-        </>
+        </div>
     )
   
 }
